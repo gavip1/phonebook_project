edbd55be0f591812110936b2d3c57ba47ba34bca
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2022-12-09 14:19:05 -0800

    Update phonebook.bash
    add functions

--- phonebook.bash
+++ phonebook.bash
@@ -6,13 +6,26 @@
 #Ask the user input for a file and check if the file exists in the current directory
 #Stop the script if the file does not exist
 
-echo "What is the filename of the phonebook?"
-read input
-FILENAME=$PWD\/$input
-if [ ! -f "$FILENAME" ]
-then
-	echo "File does not exist. hint: check if the file is in the directory or if the input file contains the right file extension"
-	exit
-fi
+look_file ()
+{
+	if [ ! -f "$1" ]
+	then
+		echo "File does not exist. hint: check if the file is in the directory or if the input file contains the right file extension"
+		return 1
+	fi
+	return 0
+}
 
+main()
+{
+	echo "What is the filename of the phonebook?"
+	read input
+	FILENAME=$PWD\/$input
+	if [ ! look_file $FILENAME ]
+	then
+		echo "do something"
+		return 0
+	fi
+}
 
+main
