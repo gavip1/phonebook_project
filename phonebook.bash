cf19ed20ef92cf0eecd6a886c696bbceedd5809c
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2022-12-09 13:51:56 -0800

    Update phonebook.bash
    change some comments

--- phonebook.bash
+++ phonebook.bash
@@ -4,7 +4,8 @@
 #Purpose: Maintain a phonebook using .bash and unix features
 
 #Ask the user input for a file and check if the file exists in the current directory
-#Close the program if the file does not exist
+#Stop the script if the file does not exist
+
 echo "What is the filename of the phonebook?"
 read input
 FILENAME=$PWD\/$input
@@ -13,4 +14,5 @@ then
 	echo "File does not exist. hint: check if the file is in the directory or if the input file contains the right file extension"
 	exit
 fi
+
 
